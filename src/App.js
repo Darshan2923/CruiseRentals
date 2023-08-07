dac468c85308fc5226881c2b135951c7ff79845d
Darshan <darshanpatel2923@gmail.com>
2023-08-07 11:59:29 +0530

    Defining routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Home from './Pages/Home';
+import Team from './Pages/Team';
+import TestimonialsPage from './Pages/TestimonialsPage';
+import Models from './Pages/Model';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="app__container">
-      Boom
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="models" element={<Models />} />
+        <Route path="testimonials" element={<TestimonialsPage />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
