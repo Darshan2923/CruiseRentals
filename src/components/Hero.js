a68f3ac55076a0cc749655e00a9de3eafcaf56f7
Darshan <darshanpatel2923@gmail.com>
2023-08-07 20:15:07 +0530

    Done with the hero section of the project

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -30,42 +30,44 @@ const Hero = () => {
         };
     }, []);
     return (
-        <section id="home" className="hero-section">
-            <div className="container">
-                <img src={BgShape} alt="bg-shape" className="bg-shape" />
-                <div className="hero-content">
-                    <div className="hero-content__text">
-                        <h4>Set your voyage in motion today</h4>
-                        <h1> Save <span>substantial</span> with <br />our offers</h1>
-                        <p>
-                            Drive your fantasy car. Unmatched rates, endless miles, adaptable pickup choices, and numerous extras.</p>
-                        <div className="hero-content__text__btns">
-                            <Link onClick={bookBtn} className='hero-content__text__btns__book-ride' to='/'>
-                                Book Ride &nbsp; <i className="fa-solid fa-circle-check"></i>
-                            </Link>
-                            <Link className='hero-content__text__btns__learn-more' to='/about'>
-                                Learn More &nbsp; <i className="fa-solid fa-angle-right"></i>
-                            </Link>
+        <>
+            <section id="home" className="hero-section">
+                <div className="container">
+                    <img src={BgShape} alt="bg-shape" className="bg-shape" />
+                    <div className="hero-content">
+                        <div className="hero-content__text">
+                            <h4>Set your voyage in motion today</h4>
+                            <h1> Save <span>substantial</span> with our offers</h1>
+                            <p>
+                                Drive your fantasy car. Unmatched rates, endless miles, adaptable pickup choices, and numerous extras.</p>
+                            <div className="hero-content__text__btns">
+                                <Link onClick={bookBtn} className='hero-content__text__btns__book-ride' to='/'>
+                                    Book Ride &nbsp; <i className="fa-solid fa-circle-check"></i>
+                                </Link>
+                                <Link className='hero-content__text__btns__learn-more' to='/about'>
+                                    Learn More &nbsp; <i className="fa-solid fa-angle-right"></i>
+                                </Link>
+                            </div>
                         </div>
+
+                        {/* image */}
+                        <img
+                            src={HeroCar}
+                            alt="car-img"
+                            className="hero-content__car-img"
+                        />
                     </div>
-
-                    {/* image */}
-                    <img
-                        src={HeroCar}
-                        alt="car-img"
-                        className="hero-content__car-img"
-                    />
                 </div>
-            </div>
 
-            {/* page up */}
-            <div
-                onClick={scrollToTop}
-                className={`scroll-up ${goUp ? "show-scroll" : ""}`}
-            >
-                <i className="fa-solid fa-angle-up"></i>
-            </div>
-        </section>
+                {/* page up */}
+                <div
+                    onClick={scrollToTop}
+                    className={`scroll-up ${goUp ? "show-scroll" : ""}`}
+                >
+                    <i className="fa-solid fa-angle-up"></i>
+                </div>
+            </section>
+        </>
 
     )
 };
