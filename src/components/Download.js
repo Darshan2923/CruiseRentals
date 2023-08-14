901c20570f4fd23ce3a61d0c84dd24ce52e2c3f0
Darshan <darshanpatel2923@gmail.com>
2023-08-14 22:24:45 +0530

    Added and completed Download.js section

--- src/components/Download.js
+++ src/components/Download.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Download = () => {
     return (
-        <div>Download</div>
+        <div>
+            <section className="download-section">
+                <div className="container">
+                    <div className="download-text">
+                        <h2>
+                            Maximize your experience by downloading our app.</h2>
+                        <p>
+                            Elevate your experience with our app download. Unlock a world of convenience and possibilities at your fingertips.
+                        </p>
+                        <div className="download-text__btns">
+                            <img src="" alt="download_img" />
+                            <img src="" alt="download_img" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
