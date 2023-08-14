600e2c815c14c2dc4088f08bb4269cc0bcb5bbcc
Darshan <darshanpatel2923@gmail.com>
2023-08-14 21:55:05 +0530

    Added heropages section to about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import HeroPages from '../components/HeroPages'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div>
+            <section className="about-page">
+                <HeroPages name="About" />
+            </section>
+        </div>
     )
 }
 
