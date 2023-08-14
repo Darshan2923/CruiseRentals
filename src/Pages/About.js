7d8040ca8110f84f77227ba2a545d0ebefc7470e
Darshan <darshanpatel2923@gmail.com>
2023-08-14 22:09:12 +0530

    Adding js to the about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,6 +6,19 @@ const About = () => {
         <div>
             <section className="about-page">
                 <HeroPages name="About" />
+                <div className="container">
+                    <div className="about-main">
+                        <img src="" alt="car-rent-img" className='about-main__img' />
+                        <div className="about-main__text">
+                            <h3>About Company</h3>
+                            <h2>Your adventure begins as you start the engine. </h2>
+                            <p>
+                                Embark on seamless journeys with our premium car rental services. Explore a diverse range of vehicles, relish competitive rates, and experience hassle-free booking. Your adventure begins with us!
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     )
