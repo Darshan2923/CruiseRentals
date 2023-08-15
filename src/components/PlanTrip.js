1955b4d7b10bed80fdc86680cbe4f1ee63d57862
Darshan <darshanpatel2923@gmail.com>
2023-08-15 17:23:57 +0530

    Added images/icons for plantrip.js

--- src/components/PlanTrip.js
+++ src/components/PlanTrip.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Icon1 from '../assets/planTrip/icon_1.jpg'
+import Icon2 from '../assets/planTrip/icon_2.jpg'
+import Icon3 from '../assets/planTrip/icon_3.jpg'
 
 const PlanTrip = () => {
     return (
@@ -12,18 +15,18 @@ const PlanTrip = () => {
                         </div>
                         <div className="plan-container__boxes">
                             <div className="plan-container__boxes__box">
-                                <img src="" alt="icon_img" />
+                                <img src={Icon1} alt="icon_img" />
                                 <h3>Choose Car</h3>
                                 <p>Explore our extensive vehicle selection, catering to all your driving requirements. Discover the ideal car tailored to your needs.</p>
                             </div>
                             <div className="plan-container__boxes__box">
-                                <img src="" alt="icon_img" />
+                                <img src={Icon2} alt="icon_img" />
                                 <h3>
                                     Call operator</h3>
                                 <p>Our operators are available to assist, addressing queries and worries with expertise and kindness.</p>
                             </div>
                             <div className="plan-container__boxes__box">
-                                <img src="" alt="icon_img" />
+                                <img src={Icon3} alt="icon_img" />
                                 <h3>Time to drive</h3>
                                 <p>
 
