53f6bca8af9f660310be61b81a03a383e0f99b9b
Darshan <darshanpatel2923@gmail.com>
2023-08-15 18:33:56 +0530

    Did a little smooth scrolling using css

--- src/components/BookCar.js
+++ src/components/BookCar.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const BookCar = () => {
     return (
-        <div>BookCar</div>
+        <>
+            <section id='book'>
+                BookCar
+            </section>
+        </>
     )
 }
 
