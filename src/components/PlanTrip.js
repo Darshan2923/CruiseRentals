ed2d23959e2558421dfe90456cf95695609234bd
Darshan <darshanpatel2923@gmail.com>
2023-08-15 16:59:27 +0530

    Added PlanTrip.js section to the project

--- src/components/PlanTrip.js
+++ src/components/PlanTrip.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const PlanTrip = () => {
     return (
-        <div>PlanTrip</div>
+        <>
+            <section className="plan-section">
+                <div className="container">
+                    <div className="plan-container">
+                        <div className="plan-container__title">
+                            <h3>Start planning your trip now.</h3>
+                            <h2>Fast and simple car rental</h2>
+                        </div>
+                        <div className="plan-container__boxes">
+                            <div className="plan-container__boxes__box">
+                                <img src="" alt="icon_img" />
+                                <h3>Choose Car</h3>
+                                <p>Explore our extensive vehicle selection, catering to all your driving requirements. Discover the ideal car tailored to your needs.</p>
+                            </div>
+                            <div className="plan-container__boxes__box">
+                                <img src="" alt="icon_img" />
+                                <h3>
+                                    Call operator</h3>
+                                <p>Our operators are available to assist, addressing queries and worries with expertise and kindness.</p>
+                            </div>
+                            <div className="plan-container__boxes__box">
+                                <img src="" alt="icon_img" />
+                                <h3>Time to drive</h3>
+                                <p>
+
+                                    For your road adventures, our diverse car collection has you protected.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
