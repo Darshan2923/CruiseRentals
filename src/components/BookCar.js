28a9a65b059dec86c86d4ac53ee44602f75c58af
Darshan <darshanpatel2923@gmail.com>
2023-08-18 17:59:58 +0530

    Did basics of BookCar.js section will do later

--- src/components/BookCar.js
+++ src/components/BookCar.js
@@ -1,10 +1,132 @@
 import React from 'react'
 
 const BookCar = () => {
+
+
+
+
+
     return (
         <>
             <section id='book'>
-                BookCar
+                {/* overlay */}
+                <div
+                    className={`modal-overlay ${modal ? "active-modal" : ""}`}
+                    onClick={openModal}></div>
+
+                <div className="container">
+                    <div className="book-content">
+                        <div className="book-content__box">
+                            <h2>Book a car</h2>
+
+                            <p className="error-message">
+                                Pls fill all fields! <i className="fa-solid fa-xmark"></i>
+                            </p>
+
+                            <p className="booking-done">
+                                Check your email to confirm an oredr.{""}
+                                <i onClick={hideMessage} className='fa-solid fa-xmark'></i>
+                            </p>
+
+                            <form className="box-form">
+                                <div className="box-form__car-type">
+                                    {/* Select car model */}
+                                    <label htmlFor='car-type__choose'>
+                                        <i className="fa-solid fa-car"></i>&nbsp; Select Your Model Type <b>*</b>
+                                    </label>
+                                    <select value={carType} name="car-type__choose" id="car-type__choose"
+                                        onChange={(e) => {
+                                            setCarType(e.target.value);
+                                            setCarImg(e.target.value);
+                                        }}>
+                                        <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
+                                        <option value="Toyota Camry">Toyota Camry</option>
+                                        <option value="Audi A1 S-Line">Audi A1 S-Line</option>
+                                        <option value="BMW 320 ModernLine">BMW 320 ModernLine</option>
+                                        <option value="VW Golf 6">VW Golf 6</option>
+                                        <option value="VW Passat CC">VW Passat CC</option>
+                                    </select>
+                                </div>
+
+                                {/* Pick-Up */}
+                                <div className="box-form__car-type">
+                                    {/* Select car model */}
+                                    <label htmlFor='pick-up__source'>
+                                        <i className="fa-solid fa-location-dot"></i>&nbsp; Pick Up{""} <b>*</b>
+                                    </label>
+                                    <select value={pickUp} name='pick-up__source' id="car-type__choose"
+                                        onChange={(e) => {
+                                            setPickUp(e.target.value);
+                                        }}>
+                                        <option>Select Pick Up Location</option>
+                                        <option>Mumbai</option>
+                                        <option>Delhi</option>
+                                        <option>Hyderabad</option>
+                                        <option>Kolkata</option>
+                                        <option>Chennai</option>
+                                        <option >Ahemdabad</option>
+                                        <option >Goa</option>
+                                        <option >Jaipur</option>
+                                    </select>
+                                </div>
+
+                                {/* Drop Off */}
+                                <div className="box-form__car-type">
+                                    {/* Select car model */}
+                                    <label htmlFor='drop-off__dest'>
+                                        <i className="fa-solid fa-location-dot"></i>&nbsp; DropOff{""} <b>*</b>
+                                    </label>
+                                    <select value={pickUp} name="drop-off__dest" id="car-type__choose"
+                                        onChange={(e) => {
+                                            setDropOff(e.target.value);
+                                        }}>
+                                        <option>Select Pick Up Location</option>
+                                        <option>Mumbai</option>
+                                        <option>Delhi</option>
+                                        <option>Hyderabad</option>
+                                        <option>Kolkata</option>
+                                        <option>Chennai</option>
+                                        <option >Ahemdabad</option>
+                                        <option >Goa</option>
+                                        <option >Jaipur</option>
+                                    </select>
+                                </div>
+
+                                {/* Pick Up Date */}
+                                <div className="box-form__car-time">
+                                    <label htmlFor="picktime">
+                                        <i className="fa-regular fa-calendar-days "></i> &nbsp;
+                                        Pick-up Date<b>*</b>
+                                    </label>
+                                    <input
+                                        id="picktime"
+                                        value={pickTime}
+                                        onChange={(e) => { setpickTime(e.target.value) }}
+                                        type="date"
+                                    ></input>
+                                </div>
+
+                                {/* Drop-Off Date */}
+                                <div className="box-form__car-time">
+                                    <label htmlFor="droptime">
+                                        <i className="fa-regular fa-calendar-days "></i> &nbsp;
+                                        Drop-off <b>*</b>
+                                    </label>
+                                    <input
+                                        id="droptime"
+                                        value={dropTime}
+                                        onChange={(e) => { setdropTime(e.target.value) }}
+                                        type="date"
+                                    ></input>
+                                </div>
+                                <button onClick={openModal} type="submit">
+                                    Search
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
             </section>
         </>
     )
