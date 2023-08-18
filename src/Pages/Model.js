69b00bb6baa3fd4ecf3350dc7281fe0465662dd3
Darshan <darshanpatel2923@gmail.com>
2023-08-18 10:35:18 +0530

    Added basic essense to the Model.js section

--- src/Pages/Model.js
+++ src/Pages/Model.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import HeroPages from '../components/HeroPages'
+import '../styles/Banner.css'
+import Footer from '../components/Footer'
 
 const Models = () => {
     return (
-        <div>
-            <HeroPages name="Models" />
-        </div>
+        <>
+            <section className="models-page">
+                <HeroPages name="Models" />
+                <div className="book-banner">
+                    <div className="book-banner__overlay"></div>
+                    <div className="container">
+                        <div className="text-content">
+                            <h2>Reserve a car by reaching out to us.</h2>
+                            <span>
+                                <i className="fa-solid fa-phone"></i>
+                                <h3>(+91) 98765 43210</h3>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </section>
+        </>
     )
 }
 
