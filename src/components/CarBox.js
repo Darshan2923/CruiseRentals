b87b6f6736dd6e2653f0f4b629241e1a1762405a
Darshan <darshanpatel2923@gmail.com>
2023-08-18 10:08:06 +0530

    Done with CarBox.js section of the project

--- src/components/CarBox.js
+++ src/components/CarBox.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const CarBox = () => {
+const CarBox = ({ data, carId }) => {
+    const [carLoad, setCarLoad] = useState(true);
     return (
-        <div>CarBox</div>
-    )
+        <>
+            {data[carId].map((car, id) => {
+                <div className="box-cars" key={id}>
+                    {/* car */}
+                    <div className="pick-car">
+                        {carLoad && <span className='loader'></span>}
+                        <img
+                            style={{ display: carLoad ? "none" : "block" }}
+                            alt="car-img"
+                            src={car.img}
+                            onLoad={() => setCarLoad(false)}
+                        />
+                    </div>
+                    {/* description */}
+                    <div className="pick-description">
+                        <div className="pick-description__price">
+                            <span>₹{car.price}</span>/rent per journey
+                        </div>
+                        <div className="pick-description__table">
+                            <div className="pick-description__table__col">
+                                <span>Year</span>
+                                <span>{car.year}</span>
+                            </div>
+                            <div className="pick-description__table__col">
+                                <span>Model</span>
+                                <span>{car.model}</span>
+                            </div>
+                            <div className="pick-description__table__col">
+                                <span>AC</span>
+                                <span>{car.AC}</span>
+                            </div>
+                            <div className="pick-description__table__col">
+                                <span>Capacity</span>
+                                <span>{car.Capacity}</span>
+                            </div>
+                            <div className="pick-description__table__col">
+                                <span>Doors</span>
+                                <span>{car.doors}</span>
+                            </div>
+                            <div className="pick-description__table__col">
+                                <span>Fuel</span>
+                                <span>{car.fuel}</span>
+                            </div>
+                        </div>
+                        {/* btn car */}
+                        <a href="#book" className="cta-btn">
+                            Reserve Now
+                        </a>
+                    </div>
+                </div>
+            })}
+        </>
+    );
 }
 
 export default CarBox
