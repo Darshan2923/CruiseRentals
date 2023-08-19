b2c510c1935371991bdf8d5366190085e41ce4c4
Darshan <darshanpatel2923@gmail.com>
2023-08-19 15:13:02 +0530

    Done with Models.js section of the project

--- src/Pages/Model.js
+++ src/Pages/Model.js
@@ -19,54 +19,17 @@ const Models = () => {
                 <div className="container">
                     <div className="models-div">
                         {/* first box */}
-                        <Models_Boxes />
-                        <div className="models-div__box">
-                            <div className="models-div__box__img">
-                                <img src={CarImg1} alt="car-img" />
-                                <div className="models-div__box__descr">
-                                    <div className="models-div__box__descr__name-price">
-                                        <div className="models-div__box__descr__name-price__name">
-                                            <p>Mercedes</p>
-                                            <span>
-                                                <i className="fa-solid fa-star"></i>
-                                                <i className="fa-solid fa-star"></i>
-                                                <i className="fa-solid fa-star"></i>
-                                                <i className="fa-solid fa-star"></i>
-                                                <i className="fa-solid fa-star"></i>
-                                            </span>
-                                        </div>
-                                        <div className="models-div__box__descr__name-price__price">
-                                            <h4>₹2499</h4>
-                                            <p>per day</p>
-                                        </div>
-                                    </div>
-                                    <div className="models-div__box__descr__name-price__details">
-                                        <span>
-                                            <i className="fa-solid fa-car-side"></i>&nbsp; Benz GLK
-                                        </span>
-                                        <span style={{ textAlign: "right" }}>
-                                            4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
-                                        </span>
-                                        <span>
-                                            <i className="fa-solid fa-car-side"></i> &nbsp; Manual
-                                        </span>
-                                        <span style={{ textAlign: "right" }}>
-                                            Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
-                                        </span>
-                                    </div>
-                                    <div className="models-div__box__descr__name-price__btn">
-                                        <Link onClick={() => window.scrollTo(0, 0)} to='/'>
-                                            Book Ride
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <Models_Boxes carImg={CarImg1} carName='Mercedes' carPrice="₹2999" carModel=" Benz GLK" carDoors="4/5" carCapacity="6 + 1 driver" carFuel="Diesel" />
                         {/* second box */}
+                        <Models_Boxes carImg={CarImg2} carName='Toyota' carPrice="₹2499" carModel=" Camry" carDoors="4/5" carCapacity="4 + 1 driver" carFuel="Hybrid" />
                         {/* third box */}
+                        <Models_Boxes carImg={CarImg3} carName='Audi A1' carPrice="₹2499" carModel=" Audi" carDoors="4/5" carCapacity="4 + 1 driver" carFuel="Gasoline" />
                         {/* fourth box */}
+                        <Models_Boxes carImg={CarImg4} carName='Bmw 320' carPrice="₹2499" carModel="320" carDoors="4/5" carCapacity="4 + 1 driver" carFuel="Diesel" />
                         {/* fifth box */}
+                        <Models_Boxes carImg={CarImg5} carName='VW Golf' carPrice="₹2199" carModel=" Golf 6" carDoors="4/5" carCapacity="4 + 1 driver" carFuel="Diesel" />
                         {/* sixth box */}
+                        <Models_Boxes carImg={CarImg6} carName='VW Passat' carPrice="₹1999" carModel="Passat CC" carDoors="4/5" carCapacity="6 + 1 driver" carFuel="Gasoline" />
                     </div>
                 </div>
 
--- src/Pages/Models_Boxes.js
+++ src/Pages/Models_Boxes.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Models_Boxes = (props) => {
   return (
@@ -19,7 +20,7 @@ const Models_Boxes = (props) => {
             </div>
             <div className="models-div__box__descr__name-price__price">
               <h4>{props.carPrice}</h4>
-              <p>{props.carPer}</p>
+              <p>per day</p>
             </div>
           </div>
           <div className="models-div__box__descr__name-price__details">
